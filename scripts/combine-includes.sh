a13a19be0e309dafb9cb5af785ec65dc24442444
Bastian Baumeister <bastian.baumeister@iteratec.com>
2022-01-13 17:07:13 +0100

    added generation of one combined include

--- scripts/combine-includes.sh
+++ scripts/combine-includes.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
+INCLUDES_TO_BE_COMBINED="colors_audi.iuml colors_base.iuml colors_iteratec.iuml style_base.iuml"
 COMBINED_INCLUDES_FILE="target/include.iuml"
 
 rm "$COMBINED_INCLUDES_FILE"
 echo "@startuml" > "$COMBINED_INCLUDES_FILE"
-tail -n+2 ressources/colors_audi.iuml | head -n -1 >> "$COMBINED_INCLUDES_FILE"
-tail -n+2 ressources/colors_iteratec.iuml | head -n -1 >> "$COMBINED_INCLUDES_FILE"
-tail -n+2 ressources/style_base.iuml | head -n -1 >> "$COMBINED_INCLUDES_FILE"
+for INCLUDE in $INCLUDES_TO_BE_COMBINED; do
+    tail -n+2 "ressources/$INCLUDE" | head -n -1 | sed "/^' help output/Q" >> "$COMBINED_INCLUDES_FILE"
+done
 echo "@enduml" >> "$COMBINED_INCLUDES_FILE"
