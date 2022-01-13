89e2240e3fd5ce79a10e451e43cd3ce42e703420
Bastian Baumeister <bastian.baumeister@iteratec.com>
2022-01-13 17:15:46 +0100

    swtiched templates to using central include

--- scripts/combine-includes.sh
+++ scripts/combine-includes.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-INCLUDES_TO_BE_COMBINED="colors_audi.iuml colors_base.iuml colors_iteratec.iuml style_base.iuml"
+INCLUDES_TO_BE_COMBINED="ressources/colors_base.iuml ressources/colors_iteratec.iuml ressources/style_base.iuml ressources/icons.iuml"
 COMBINED_INCLUDES_FILE="target/include.iuml"
 
 rm "$COMBINED_INCLUDES_FILE"
 echo "@startuml" > "$COMBINED_INCLUDES_FILE"
 for INCLUDE in $INCLUDES_TO_BE_COMBINED; do
-    tail -n+2 "ressources/$INCLUDE" | head -n -1 | sed "/^' help output/Q" >> "$COMBINED_INCLUDES_FILE"
+    echo "' $INCLUDE" >> "$COMBINED_INCLUDES_FILE"
+    tail -n+2 "$INCLUDE" | head -n -1 | sed "/^' help output/Q" >> "$COMBINED_INCLUDES_FILE"
+    echo "" >> "$COMBINED_INCLUDES_FILE"
 done
 echo "@enduml" >> "$COMBINED_INCLUDES_FILE"
